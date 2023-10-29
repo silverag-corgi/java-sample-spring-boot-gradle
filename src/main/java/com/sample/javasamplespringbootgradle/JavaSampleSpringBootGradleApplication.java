808bd74c774947e6d8f3a699caa0209b639520ba
Rem_ <black.white.red.are.the.best@gmail.com>
2023-10-29 21:27:05 +0900

    feat: サンプルを作成

---
+++ src/main/java/com/sample/javasamplespringbootgradle/JavaSampleSpringBootGradleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.javasamplespringbootgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSampleSpringBootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSampleSpringBootGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/javasamplespringbootgradle/JavaSampleSpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.javasamplespringbootgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSampleSpringBootGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
